0df0b7eefb8ff834e2a9c93adac5b31b5b289d10
Brahathees <brahatheeswaranparimalam@gmail.com>
2026-05-14 03:37:58 +0000

    feat: change color to green v2

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const color = process.env.COLOR || 'blue';
-const version = process.env.VERSION || 'v1';
+const color = process.env.COLOR || 'green';
+const version = process.env.VERSION || 'v2';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
